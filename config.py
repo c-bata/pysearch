95c80c387da379f96eb0242bc86d87432af60d78
mejiro <m.sh.nwpct1@gmail.com>
2014-11-11 00:08:22 +0900

    MongoDB使うための準備

---
+++ config.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+__author__ = 'masashi'
+
+import os
+
+# application settings
+#MONGO_URL = ''
+
+# Generate a random secret key
+SECRET_KEY = os.urandom(24)
+CSRF_ENABLED = True
