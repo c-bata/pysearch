25409a4590fdd3976930639f404093b4896ea0fe
mejiro <m.sh.nwpct1@gmail.com>
2015-02-06 14:25:22 +0900

    Refactor search_engine application

--- search_engine/__init__.py
+++ search_engine/__init__.py
@@ -1,17 +1,15 @@
 # -*- coding: utf-8 -*-
-__author__ = 'masashi'
 
 from flask import Flask
 from pymongo import MongoClient
 from urlparse import urlparse
+from search_engine import views
 
 app = Flask(__name__)
 app.config.from_object('config')
 
-
+# DB settings
 MONGO_URL = app.config['MONGO_URL']
 client = MongoClient(MONGO_URL)
 db = client[urlparse(MONGO_URL).path[1:]]
 collection = db["Index"]
-
-from search_engine import views
--- search_engine/views.py
+++ search_engine/views.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-__author__ = 'masashi'
 
 from search_engine import app, collection as col
 from flask import render_template, request
@@ -11,7 +10,8 @@ def index():
     if request.method == 'POST':
         keyword = request.form['keyword']
         if keyword:
-            return render_template('index.html',
-                                   query=col.find_one({'keyword': keyword}),
-                                   keyword=keyword)
+            return render_template(
+                'index.html',
+                query=col.find_one({'keyword': keyword}),
+                keyword=keyword)
     return render_template('index.html')
